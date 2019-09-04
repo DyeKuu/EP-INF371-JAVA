fc31d33595bca89c0594a414a55b0dac1812c6b4
DyeKuu <39208702+DyeKuu@users.noreply.github.com>
2019-09-04 17:01:29 +0200

    TD1

---
+++ test.java
@@ -0,0 +1,13 @@
+public class test{
+    public static void main(String a){
+        System.out.println(1);
+    }
+
+    public static void main(int s){
+        System.out.println(2);
+    }
+    
+    public static void main(String[] args){
+        System.out.println(3);
+    }
+}
